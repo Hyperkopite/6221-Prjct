4c0da5d6ee7ca9703517d50203f275fff53294e0
root <root@localhost.localdomain>
2018-11-01 12:24:03 -0400

    Add init.py

---
+++ init.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#This module is used to find out is everything needed have been
+#properly installed and ready to run.
+#If not, run script automatically to install what missed.
+
+def main():
+    print("Init!")
+
+
+if __name__ == '__main__':
+    main()
